296064f890b0a65d1ae6a87612656c0e4f7b0c86
chenbo <chenbo@huaweisoft.com>
2025-02-11 16:04:33 +0800

    重新发起实现

--- workflow/demo-flowable/src/test/java/com/github/wugenshui/flowable/demo/RedoTest.java
+++ workflow/demo-flowable/src/test/java/com/github/wugenshui/flowable/demo/RedoTest.java
@@ -6,12 +6,14 @@ import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
+import org.flowable.engine.history.HistoricProcessInstance;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.task.api.Task;
 import org.flowable.task.api.TaskInfo;
 import org.flowable.task.api.history.HistoricTaskInstance;
+import org.flowable.variable.api.history.HistoricVariableInstance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -36,7 +38,7 @@ class RedoTest {
     @Resource
     private HistoryService historyService;
 
-    // 普通流程完成 [Approve or reject request] -> [Holiday approved]
+    // 重新发起实现 [Approve or reject request] -> [Holiday approved]
     @Test
     void redo() {
         RepositoryService repositoryService = processEngine.getRepositoryService();
@@ -58,7 +60,7 @@ class RedoTest {
         variables.put("nrOfHolidays", 1);
         variables.put("description", "请假");
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("holidayRequest", variables);
-        System.out.printf("发起流程实例（processInstance） Id:%s \n", processInstance.getId());
+        System.out.printf("----> 发起流程实例（processInstance） [%s] \n", processInstance.getId());
 
         // 查询任务
         List<Task> tasks = printCurrentTask();
@@ -72,13 +74,44 @@ class RedoTest {
         tasks = printCurrentTask();
 
         // 取消任务
-        runtimeService.deleteProcessInstance(processInstance.getId(), "取消流程");
-        System.out.printf("--> 取消流程：%s \n", tasks.get(0).getName());
+        runtimeService.deleteProcessInstance(processInstance.getId(), "取消流程实例");
+        System.out.printf("--> 取消流程实例：%s \n", tasks.get(0).getName());
 
-        // 查询历史
-        List<HistoricTaskInstance> history = historyService.createHistoricTaskInstanceQuery().processInstanceId(processInstance.getId()).list();
+        // 重新发起
+        ProcessInstance processInstance2 = runtimeService.startProcessInstanceByKey("holidayRequest", variables);
+        System.out.printf("----> 发起新的流程实例（processInstance） [%s] \n", processInstance2.getId());
+        // 关联至旧的流程实例
+        runtimeService.setVariable(processInstance2.getId(), "LAST_HANDLE_RUNTIME_ID", processInstance.getId());
+
+        // 查询历史实例
+        List<HistoricProcessInstance> total = historyService.createHistoricProcessInstanceQuery().list();
+        List<HistoricProcessInstance> part = historyService.createHistoricProcessInstanceQuery().variableExists("LAST_HANDLE_RUNTIME_ID").list();
+        System.out.printf("HistoricProcessInstance total=%s part=%s  \n", total.size(), part.size());
+
+        // 查询历史任务
+        List<HistoricTaskInstance> history = historyService.createHistoricTaskInstanceQuery()
+                .processInstanceId(part.get(0).getId())
+                .list();
         List<String> names = history.stream().map(TaskInfo::getName).collect(Collectors.toList());
-        System.out.printf("执行历史：%s", String.join(" --> ", names));
+        System.out.printf("执行历史：[%s] %s \n", part.get(0).getId(), String.join(" --> ", names));
+
+        // 通过变量查询关联的旧任务
+        HistoricVariableInstance variable = historyService.createHistoricVariableInstanceQuery()
+                .processInstanceId(part.get(0).getId())
+                .variableName("LAST_HANDLE_RUNTIME_ID")
+                .singleResult();
+        if (variable != null) {
+            String value = (String) variable.getValue();
+            System.out.println("Variable LAST_HANDLE_RUNTIME_ID=" + value);
+            List<HistoricTaskInstance> history2 = historyService.createHistoricTaskInstanceQuery()
+                    .processVariableNotExists("LAST_HANDLE_RUNTIME_ID")
+                    .processInstanceId(value)
+                    .list();
+            List<String> names2 = history2.stream().map(TaskInfo::getName).collect(Collectors.toList());
+            System.out.printf("执行历史2：[%s] %s \n", value, String.join(" --> ", names2));
+        } else {
+            System.out.println("Variable not found.");
+        }
     }
 
     private List<Task> printCurrentTask() {
