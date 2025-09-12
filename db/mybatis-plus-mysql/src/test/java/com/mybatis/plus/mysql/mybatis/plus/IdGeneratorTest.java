dd4fcda5fe651910d30cec9fa191908572636c03
chenbo <963838470@qq.com>
2025-09-12 11:41:42 +0800

    IdGeneratorTest

---
+++ db/mybatis-plus-mysql/src/test/java/com/mybatis/plus/mysql/mybatis/plus/IdGeneratorTest.java
@@ -0,0 +1,21 @@
+package com.mybatis.plus.mysql.mybatis.plus;
+
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.core.toolkit.Sequence;
+
+/**
+ * @author : chenbo
+ * @date : 2025-09-12
+ */
+public class IdGeneratorTest {
+    public static void main(String[] args) {
+        // 推荐使用，19位，顺序
+        for (int i = 0; i < 10; i++) {
+            System.out.println("nextId = " + new Sequence(null).nextId());
+        }
+        // 长度太长，适合做分布式ID
+        for (int i = 0; i < 10; i++) {
+            System.out.println("IdWorker.get32UUID() = " + IdWorker.get32UUID());
+        }
+    }
+}
